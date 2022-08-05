7d3ce8685c218e7e3df3e0f5d828f74c37ad2044
TrucNguyennn <truc.nguyen@zodinet.tech>
2022-08-05 14:35:09 +0700

    feat: login gg

--- src/pages/otp.tsx
+++ src/pages/otp.tsx
@@ -1,25 +1,37 @@
 import { loginOtp } from '@/services/login.api';
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import { googleProvider } from '@/config/providers';
+import { getAuth, signInWithPopup } from 'firebase/auth';
+import { firebaseApp } from '@/config/firebase-config';
+import { useContext } from '@/context/UserContext';
+import { Button } from 'antd';
+import type { NextPage } from 'next';
 
-const Otp: React.FC = () => {
+const Otp: NextPage = () => {
   const router = useRouter();
   const [phone, setPhone] = useState<string>(`2633378748`);
   const [code, setCode] = useState<string>(`777777`);
+  const firebaseAuth = getAuth(firebaseApp);
+  const { verifyUser } = useContext();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     if ((phone.length !== 0, code.length !== 0)) {
       const data = await loginOtp({ phone: phone, code: code });
       localStorage.setItem(`access-token`, data.data.data.token);
       router.push(`/home`);
     }
   };
-
+  const onSignInGoogle = async () => {
+    const { user } = await signInWithPopup(firebaseAuth, googleProvider);
+    const result = await verifyUser(user.providerData[0]);
+    if (result.isNewUser) router.push(`/register`);
+    else router.push(`/`);
+  };
   return (
     <section>
       <h1>Login with OTP</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <input
           type="text"
           placeholder="Phone Number"
@@ -32,9 +44,12 @@ const Otp: React.FC = () => {
           value={code}
           onChange={(e) => setCode(e.target.value)}
         />
-        <button type="submit" onClick={() => handleSubmit}>
+        <Button type="primary" htmlType="submit" onClick={handleSubmit}>
           Login
-        </button>
+        </Button>
+        <Button type="primary" htmlType="submit" onClick={onSignInGoogle}>
+          Login with google
+        </Button>
       </form>
     </section>
   );
