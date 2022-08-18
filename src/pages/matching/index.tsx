3b9f357d6a741ff0e4747350d33fe7bcc0e79fe0
Thinh Dang <thinh-dang@zodinet.tech>
2022-08-18 12:04:32 +0700

    fix: matched friends

--- src/pages/matching/index.tsx
+++ src/pages/matching/index.tsx
@@ -1,23 +1,50 @@
 import { url } from 'inspector';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HeartContainer from './components/heart-container';
-
+export interface IMatchedFriends {
+  id: string;
+  imgUrl: string;
+}
 const MatchPage = () => {
   const [greetMessage, setGreetMessage] = useState('');
+  const [matchedFriends, setMatchedFriends] = useState([
+    { id: '1', imgUrl: './assets/images/avatar/avatar1.jpg' },
+    { id: '2', imgUrl: './assets/images/avatar/avatar2.jpg' },
+    { id: '3', imgUrl: './assets/images/avatar/avatar3.jpg' },
+    { id: '4', imgUrl: './assets/images/avatar/avatar4.jpg' },
+    { id: '5', imgUrl: './assets/images/avatar/avatar5.jpg' },
+    { id: '6', imgUrl: './assets/images/avatar/avatar6.jpg' },
+    { id: '7', imgUrl: './assets/images/avatar/avatar7.jpg' },
+    { id: '8', imgUrl: './assets/images/avatar/avatar8.jpg' },
+    { id: '9', imgUrl: './assets/images/avatar/avatar9.jpg' },
+    { id: '10', imgUrl: './assets/images/avatar/avatar10.jpg' },
+  ]);
   const handleOnChange = (e: any) => {
     setGreetMessage(e.target.value);
     console.log(greetMessage);
   };
+  const handleSkip = () => {
+    const arr = [...matchedFriends];
+
+    if (arr.length < 2) {
+      window.location.href = './chat';
+      return;
+    }
+
+    arr.shift();
+    setMatchedFriends(arr);
+    console.log(matchedFriends);
+  };
   return (
     <div className="matchingFrame">
-      <a href="#">
+      <a href="./chat">
         <img
           className="matchingFrame-btnClose"
           src="./assets/images/CloseBtn.svg"
           alt=""
         />
       </a>
-      <HeartContainer />
+      <HeartContainer matchedFriend={matchedFriends && matchedFriends[0]} />
       <div className="matchingFrame-content">
         <h2 className="matchingFrame-content-title">IT'S A MATCH</h2>
         <p className="matchingFrame-content-message">
@@ -40,7 +67,9 @@ const MatchPage = () => {
             className="matchingFrame-input-send"
           ></button>
         </form>
-        <button className="matchingFrame-skip">Skip</button>
+        <button onClick={handleSkip} className="matchingFrame-skip">
+          Skip
+        </button>
       </div>
     </div>
   );
